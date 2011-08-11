4c81b9806ea3b52d85b62d7c5354c7c751203e64
James FitzGibbon <james@nadt.net>
2011-08-10 17:15:21 -0700

    finish Digits rewrite

--- lib/WoWUI/Module/Digits.pm
+++ lib/WoWUI/Module/Digits.pm
@@ -26,76 +26,77 @@ sub BUILD
     my $self = shift;
     
     $self->global( 1 );
+    $self->globalpc( 1 );
     
     return $self;
 
 }
 
-sub augment_data
+sub augment_global
 {
 
     my $self = shift;
+
+    my $config = $self->config;
+    my $o = $self->modoptions;
+    
+    if( $self->has_globaldata ) {
+        $self->globaldata_set( font => $o->{font} );
+        $self->globaldata_set( anchor => $o->{anchor} );
+        $self->globaldata_set( powertypes => $config->{powertypes} );
+    }
+
+    return $self;
+
+}
+
+sub augment_globalpc
+{
+
+    my $self = shift;
+    my $char = shift;
+    my $f = shift;
 
     my $log = WoWUI::Util->log;
 
     my $config = $self->config;
     my $o = $self->modoptions;
 
-    my $data;
-      
-    # per-char settings
-    for my $realm( $self->player->realms ) {
-        $log->debug("processing realm ", $realm->name);
-        for my $char( $realm->chars ) {
-
-            if( exists $config->{perchar_criteria} ) {
-                next unless( WoWUI::Util::Filter::matches( $char->flags_get('all'), $char, $config->{perchar_criteria} ) );
+    # frames enabled
+    my %ft_enabled;
+    for my $frame( keys %{ $config->{frames} } ) {
+        my $fn = $config->{frames}->{$frame}->{name};
+        $self->globaldata->{realms}->{$f->char->realm->name}->{$f->char->name}->{frames}->{$fn} ||= {
+            enabled => 0,
+            percent => 0,
+            ooc => 0
+        };
+        if( $f->match( $config->{frames}->{$frame}->{filter} ) ) {
+            if( exists $ft_enabled{$fn} ) {
+                croak "double match on $fn for ", $char->rname, ": $ft_enabled{$fn} before $frame";
             }
-
-            # frames enabled
-            my %ft_enabled;
-            for my $f( keys %{ $config->{frames} } ) {
-                my $fn = $config->{frames}->{$f}->{name};
-                $data->{realms}->{$realm->name}->{$char->name}->{frames}->{$fn} ||= {
-                    enabled => 0,
-                    percent => 0,
-                    ooc => 0
-                };
-                if( WoWUI::Util::Filter::matches( $char->flags_get('all'), $char, $config->{frames}->{$f} ) ) {
-                    if( exists $ft_enabled{$fn} ) {
-                        croak "double match on $fn for ", $char->rname, ": $ft_enabled{$fn} before $f";
-                    }
-                    else {
-                        $ft_enabled{$fn} = $f;
-                    }
-                    $data->{realms}->{$realm->name}->{$char->name}->{frames}->{$fn} = {
-                        enabled => 1,
-                        percent => $config->{frames}->{$f}->{percent},
-                        ooc => $config->{frames}->{$f}->{ooc},
-                    }
-                }
+            else {
+                $ft_enabled{$fn} = $frame;
             }
-
-            # power types
-            for my $p( keys %{ $config->{powertypes} } ) {
-                if( WoWUI::Util::Filter::matches( $char->flags_get('all'), $char, $config->{powertypes}->{$p} ) ) {
-                    $data->{realms}->{$realm->name}->{$char->name}->{$p} = 1;
-                }
-                else {
-                    $data->{realms}->{$realm->name}->{$char->name}->{$p} = 0;
-                }
+            $self->globaldata->{realms}->{$f->char->realm->name}->{$f->char->name}->{frames}->{$fn} = {
+                enabled => 1,
+                percent => $config->{frames}->{$frame}->{percent},
+                ooc => $config->{frames}->{$frame}->{ooc},
             }
-
         }
     }
 
-    if( %{ $data->{realms} } ) {
-        $data->{font} = $o->{font};
-        $data->{anchor} = $o->{anchor};
-        $data->{powertypes} = $config->{powertypes};
+    # power types
+    for my $p( keys %{ $config->{powertypes} } ) {
+        if( $f->match( $config->{powertypes}->{$p}->{filter} ) ) {
+            $self->globaldata->{realms}->{$f->char->realm->name}->{$f->char->name}->{$p} = 1;
+        }
+        else {
+            $self->globaldata->{realms}->{$f->char->realm->name}->{$f->char->name}->{$p} = 0;
+        }
     }
 
-    return $data;
+    return $self;
 
 }
 
