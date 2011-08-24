fb45dea3126e5f0367fe194f05add228bef19be1
James FitzGibbon <james@nadt.net>
2011-08-24 15:33:17 -0700

    finish Kong rewrite

--- lib/WoWUI/Module/Kong.pm
+++ lib/WoWUI/Module/Kong.pm
@@ -18,6 +18,7 @@ use Set::Scalar;
 
 use WoWUI::Config;
 use WoWUI::Util 'log';
+use WoWUI::Filter::Constants;
 
 # constructor
 sub BUILD
@@ -25,78 +26,71 @@ sub BUILD
 
     my $self = shift;
 
-    $self->global( 1 );
+    $self->globalpc( 1 );
     $self->perchar( 1 );
     
     return $self;
 
 }
 
-sub augment_data
+sub augment_globalpc
 {
 
     my $self = shift;
+    my $char = shift;
+    my $f = shift;
 
     my $log = WoWUI::Util->log;
+    my $config = $self->modconfig( $char );
 
-    my $config = $self->modconfig;
-
-    my $data;
-
-    for my $realm( $self->player->realms ) {
-        $log->debug("processing realm ", $realm->name);
-        for my $char( $realm->chars ) {
-
-            if( exists $config->{perchar_criteria} ) {
-                next unless( WoWUI::Util::Filter::matches( $char->flags_get('all'), $char, $config->{perchar_criteria} ) );
-            }
-            $log->debug("processing character ", $char->name);
-
-            # calculate profile name
-            my $profilename = $char->rname;
-            $profilename =~ tr/A-Z/a-z/;
-            $profilename =~ s/\s/_/g;
-
-            # build list of frames
-            my @frames;
-            my %groups;
-            for my $frame( keys %{ $config->{frames} } ) {
-                if( exists $config->{frames}->{$frame}->{criteria} ) {
-                    if( WoWUI::Util::Filter::matches( $char->flags_get('all'), $char, $config->{frames}->{$frame}->{criteria} ) ) {
-                        push @frames, $config->{frames}->{$frame};
-                        if( my $group = $config->{frames}->{$frame}->{group} ) {
-                            push @{ $groups{$group} }, $config->{frames}->{$frame}->{name};
-                        }
-                    }
-                }
-            }
-            $data->{kong}->{$realm->name}->{$char->name} = {
-                profilename => $profilename,
-                frames => \@frames,
-            };
-            if( %groups ) {
-                for my $group( keys %groups ) {
-                    push @{ $data->{kong}->{$realm->name}->{$char->name}->{groups} }, { name => $group, frames => $groups{$group} };
+    # build list of frames
+    my @frames;
+    my %groups;
+    for my $frame( keys %{ $config->{frames} } ) {
+        if( exists $config->{frames}->{$frame}->{filter} ) {
+            if( $f->match( $config->{frames}->{$frame}->{filter}, F_ALL ) ) {
+                push @frames, $config->{frames}->{$frame};
+                if( my $group = $config->{frames}->{$frame}->{group} ) {
+                    push @{ $groups{$group} }, $config->{frames}->{$frame}->{name};
                 }
             }
         }
     }
-
-    return $data;
+    $self->globaldata->{kong}->{$char->realm->name}->{$char->name} = {
+        profilename => $self->profilename( $char ),
+        frames => \@frames,
+    };
+    if( %groups ) {
+        for my $group( keys %groups ) {
+            push @{ $self->globaldata->{kong}->{$char->realm->name}->{$char->name}->{groups} },
+                { name => $group, frames => $groups{$group} };
+        }
+    }
 
 }
 
-sub augment_chardata
+sub augment_perchar
 {
 
   my $self = shift;
   my $char = shift;
+  my $f = shift;
 
-  my $profilename = $char->rname;
-  $profilename =~ tr/A-Z/a-z/;
-  $profilename =~ s/\s/_/g;
+  $self->perchardata_set( profilename => $self->profilename( $char ) );
 
-  return { realm => $char->realm->name, char => $char->name, profilename => $profilename };
+}
+
+sub profilename
+{
+
+    my $self = shift;
+    my $char = shift;
+    
+    my $profilename = $char->rname;
+    $profilename =~ tr/A-Z/a-z/;
+    $profilename =~ s/\s/_/g;
+    
+    return $profilename;
 
 }
 
