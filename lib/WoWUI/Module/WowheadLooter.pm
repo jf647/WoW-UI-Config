f3c446d5a9dd83e2ec89d2c7675db557fcbbc020
James FitzGibbon <james@nadt.net>
2011-10-16 11:08:13 -0700

    add new module for WowheadLooter

---
+++ lib/WoWUI/Module/WowheadLooter.pm
@@ -0,0 +1,50 @@
+#
+# $Id: Hydra.pm 4998 2011-05-25 16:26:54Z james $
+#
+
+package WoWUI::Module::WowheadLooter;
+use Moose;
+use MooseX::StrictConstructor;
+
+use CLASS;
+use namespace::autoclean;
+
+# set up class
+extends 'WoWUI::Module::Base';
+CLASS->meta->make_immutable;
+
+use Clone 'clone';
+use Carp 'croak';
+
+use WoWUI::Config;
+use WoWUI::Util 'log';
+use WoWUI::Filter::Constants;
+
+# constructor
+sub BUILD
+{
+
+    my $self = shift;
+    
+    $self->perchar( 1 );
+    
+    return $self;
+    
+}
+
+sub augment_perchar
+{
+
+    my $self = shift;
+    my $char = shift;
+    my $f = shift;
+    
+    $self->perchardata_set( wowheaderlooter => 1 );
+
+}
+
+# keep require happy
+1;
+
+#
+# EOF
