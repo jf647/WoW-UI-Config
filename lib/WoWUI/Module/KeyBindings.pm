578fb4a33ab54226e87a1345e705a7f45ae3033a
James FitzGibbon <james@nadt.net>
2011-10-29 14:38:48 -0700

    finish new Keybindings module (only tested global implementation)

--- lib/WoWUI/Module/KeyBindings.pm
+++ lib/WoWUI/Module/KeyBindings.pm
@@ -2,7 +2,7 @@
 # $Id$
 #
 
-package WoWUI::Module::Addons;
+package WoWUI::Module::KeyBindings;
 use Moose;
 use MooseX::StrictConstructor;
 
@@ -21,54 +21,169 @@ use WoWUI::Util 'log';
 sub BUILD
 {
 
-  my $self = shift;
+    my $self = shift;
 
-  my $config = $self->config;
+    my $config = $self->config;
   
-  $self->global( 1 );
-  $self->perchar( 1 );
+    $self->global( 1 );
+    $self->perchar( 1 );
 
-  return $self;
+    return $self;
 
 }
 
 sub augment_global
 {
 
-  my $self = shift;
+    my $self = shift;
 
-  my $config = $self->modconfig;
+    my $config = $self->modconfig;
   
-  # iterate over keys
-  
-  # unbind keys
-  
-  # iterate over modified clicks
+    if( $config->{bindingmode} eq 'global' ) {
+        $self->augment_global_global;
+    }
+    elsif( $config->{bindingmode} eq 'perchar' ) {
+        $self->augment_global_perchar;
+    }
+    else {
+        croak "invalid binding mode '$config->{bindingmode}'";
+    }
 
 }
 
 sub augment_perchar
 {
 
-  my $self = shift;
-  my $char = shift;
-  my $f = shift;
+    my $self = shift;
+    my $char = shift;
+    my $f = shift;
 
-  my $config = $self->modconfig( $char );
-  my $log = WoWUI::Util->log;
+    my $config = $self->modconfig;
+
+    if( $config->{bindingmode} eq 'global' ) {
+        $self->augment_perchar_global;
+    }
+    elsif( $config->{bindingmode} eq 'perchar' ) {
+        $self->augment_perchar_perchar;
+    }
+    else {
+        croak "invalid binding mode '$config->{bindingmode}'";
+    }
+
+}
+
+
+sub augment_global_global
+{
+    my $self = shift;
+    my $config = $self->modconfig;
+
+    my %data;
+    $self->build_all( \%data, $config );
+    $self->globaldata_set( bindingmode => 'global', %data );
+
+}
+
+sub augment_perchar_perchar
+{
+
+    my $self = shift;
+    my $char = shift;
+    my $config = $self->modconfig( $char );
+
+    my %data;
+    $self->build( \%data, $config );
+    $self->perchardata_set( %data );
+
+}
+
+sub augment_global_perchar
+{
+
+    my $self = shift;
+    
+    $self->globaldata_set( bindingmode => 'perchar' );
+
+}
+
+sub augment_perchar_global
+{
+
+    my $self = shift;
+    
+    $self->perchardata_set( bindingmode => 'global' );
+
+}
+
+sub build_all
+{
+
+    my $self = shift;
+    my $data = shift;
+    my $config = shift;
+    
+    $self->build_bindings($data, $config);
+    $self->unbind_keys($data, $config);
+    $self->build_modifiedclicks($data, $config);
 
 }
 
 sub build_bindings
 {
 
+    my $self = shift;
+    my $data = shift;
     my $config = shift;
 
     my %bindings;
     
-    for my $binding( keys %{ $config->{bind}
+    for my $binding( keys %{ $config->{bind} } ) {
+        my $key = $config->{bind}->{$binding};
+        if( exists $config->{aliases}->{$binding} ) {
+            $binding = $config->{aliases}->{$binding};
+        }
+        if( ref $key ) {
+            for my $k( @$key ) {
+                $bindings{$k} = $binding;
+            }
+        }
+        else {
+            $bindings{$key} = $binding;
+        }
+    }
 
-    return \%bindings;
+    $data->{bindings} = \%bindings;
+
+}
+
+sub unbind_keys
+{
+
+    my $self = shift;
+    my $data = shift;
+    my $config = shift;
+
+    for my $unbind( @{ $config->{unbind} } ) {
+        $data->{bindings}->{$unbind} = 'NONE';
+    }
+
+}
+
+sub build_modifiedclicks
+{
+
+    my $self = shift;
+    my $data = shift;
+    my $config = shift;
+    
+    my %modifiedclicks;
+    
+    for my $mod( keys %{ $config->{modifiedclick} } ) {
+        my $key = $config->{modifiedclick}->{$mod};
+        $modifiedclicks{$key} = $mod;
+    }
+    
+    $data->{modifiedclicks} = \%modifiedclicks;
 
 }
 
