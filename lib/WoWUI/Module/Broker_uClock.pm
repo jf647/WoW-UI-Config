a36fe3d485fa9499297b7043b220065cd0a01d8d
James FitzGibbon <james@nadt.net>
2012-01-06 20:02:04 -0800

    add Broker_uClock module

---
+++ lib/WoWUI/Module/Broker_uClock.pm
@@ -0,0 +1,37 @@
+package WoWUI::Module::Broker_uClock;
+use Moose;
+use MooseX::StrictConstructor;
+
+use CLASS;
+use namespace::autoclean;
+
+# set up class
+extends 'WoWUI::Module::Base';
+CLASS->meta->make_immutable;
+
+# constructor
+sub BUILD
+{
+
+    my $self = shift;
+    
+    $self->global( 1 );
+    $self->globalpc( 1 );
+    
+    return $self;
+
+}
+
+sub augment_global
+{
+
+    my $self = shift;
+
+    my $config = $self->modconfig;
+    
+    $self->globaldata_set( options => $config->{broker_uclock} );
+
+}
+
+sub augment_globalpc { return 1 }
+
