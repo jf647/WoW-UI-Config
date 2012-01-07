223b38f0172392655a1e96bcd4e036f3af458e7a
James FitzGibbon <james@nadt.net>
2012-01-06 20:19:22 -0800

    add Mapster module

---
+++ lib/WoWUI/Module/Mapster.pm
@@ -0,0 +1,36 @@
+package WoWUI::Module::Mapster;
+use Moose;
+use MooseX::StrictConstructor;
+
+use CLASS;
+use namespace::autoclean;
+
+# set up class
+extends 'WoWUI::Module::Base';
+CLASS->meta->make_immutable;
+
+# constructor
+sub BUILD
+{
+
+    my $self = shift;
+    
+    $self->global( 1 );
+    $self->globalpc( 1 );
+    
+    return $self;
+
+}
+
+sub augment_global
+{
+
+    my $self = shift;
+
+    my $config = $self->modconfig;
+    
+    $self->globaldata_set( mapster => $config->{mapster} );
+
+}
+
+sub augment_globalpc { return 1 }
