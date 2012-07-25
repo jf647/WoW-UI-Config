33bb051d336aa253676d48e4a54e99c7903d05dc
James FitzGibbon <james@nadt.net>
2012-07-25 13:36:20 -0700

    ...

---
+++ lib/WoWUI/Module/Dominos.pm
@@ -0,0 +1,107 @@
+package WoWUI::Module::Dominos;
+use Moose;
+use MooseX::StrictConstructor;
+
+use CLASS;
+use namespace::autoclean;
+
+use WoWUI::ProfileSet;
+use WoWUI::Filter::Constants;
+
+# set up class
+extends 'WoWUI::Module::Base';
+has profileset => (
+    is => 'rw',
+    isa => 'WoWUI::ProfileSet',
+    default => sub { WoWUI::ProfileSet->new },
+);
+after 'globaldata_clear' => sub {
+    my $self = shift;
+    $self->globaldata_set( chars => {} );
+};
+CLASS->meta->make_immutable;
+
+use Carp 'croak';
+
+use WoWUI::Config;
+use WoWUI::Util 'log';
+
+# constructor
+sub BUILD
+{
+    
+    my $self = shift;
+    
+    $self->global( 1 );
+    $self->globalpc( 1 );
+    $self->globaldata_clear;
+    
+    return $self;
+    
+}
+
+sub augment_global
+{
+
+    my $self = shift;
+
+    $self->globaldata_set( dominos => $self );
+
+}
+
+sub augment_globalpc
+{
+
+    my $self = shift;
+    my $char = shift;
+    my $f = shift;
+
+    my $profile = $self->build_profile( $char, $f );
+    
+    my $pname = $self->profileset->store( $profile, 'Dominos' );
+    $self->globaldata_get( 'chars' )->{$char->dname} = $pname;
+
+}
+
+sub build_profile
+{
+
+    my $self = shift;
+    my $char = shift;
+    my $f = shift;
+    
+    my $config = $self->modconfig( $char );
+
+    my $profile;
+    
+    # simple sections
+    for my $section( qw|showminimap linkedopacity barscale| ) {
+        for my $block( values %{ $config->{$section} } ) {
+        }
+        
+    }
+    
+    # generic
+    for my $type( qw|frame override| ) {
+        for my $block( values %{ $config->{$type} } ) {
+            $profile->{"has_$type"} = 0;
+            if( $f->match( $block->{filter}, F_CALL|F_MACH ) ) {
+                $profile->{$type} = $block->{$type};
+                $profile->{"has_$type"} = 1;
+                last;
+            }
+        }
+    }
+
+    # rules
+    for my $blockname( keys %{ $config->{rules} } ) {
+        my $block = $config->{rules}->{$blockname};
+        $block->{rule}->{name} = $blockname;
+        if( $f->match( $block->{filter}, F_CALL|F_MACH ) ) {
+            push @{ $profile->{rules} }, $block->{rule};
+        }
+    }
+    
+    return $profile;
+
+}
